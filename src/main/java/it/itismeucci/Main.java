d2746970e2193312981a5bf36abcfe7b2435ab21
tommipiz <tommaso.pizzutilo@gmail.com>
2024-10-09 09:14:59 +0200

    Client completed

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,29 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket s = new Socket("localhost", 5637);
+        System.out.println("Il client si è collegato");
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        out.writeBytes("Client" + "\n");
+
+        String stringRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringRicevuta);
+
+        s.close();
+
+
+
     }
 }
